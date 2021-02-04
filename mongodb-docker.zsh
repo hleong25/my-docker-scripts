e06d4da39798cc62c0d5bcafe10bf473f731fef9
henry leong <hleong25+github@gmail.com>
2021-02-03 22:19:52 -0800

    add mongodb

---
+++ mongodb-docker.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+compose_name=mongo
+
+case "$1" in
+up) 
+    docker stack deploy --compose-file mongodb-stack.yml $compose_name 
+    echo
+    echo go to http://127.0.0.1:8081
+    ;;
+down) 
+    docker stack rm $compose_name 
+    ;;
+*) 
+    echo "$0 [up|down]"
+    ;;
+esac
