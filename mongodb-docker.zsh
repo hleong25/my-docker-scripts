a34cd57480ae04ba1129624214ecb3fbb2d5eec9
henry leong <hleong25+github@gmail.com>
2021-02-05 00:12:59 -0800

    open mongodb port, have shared drive, run script from anywhere

--- mongodb-docker.zsh
+++ mongodb-docker.zsh
@@ -4,6 +4,8 @@ compose_name=mongo
 
 case "$1" in
 up) 
+    current_dir=`dirname $0`
+    chdir $current_dir
     docker stack deploy --compose-file mongodb-stack.yml $compose_name 
     echo
     echo go to http://127.0.0.1:8081
